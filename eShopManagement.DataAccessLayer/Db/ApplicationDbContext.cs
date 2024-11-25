91451d86cb494cb13c1d9b9090c9095a4f82c758
hdhuydev <hdhuydev@gmail.com>
2024-11-25 08:30:01 +0700

    Add product, category entities, entity configurations, interfaces, repositories, generate Dbcontext

--- eShopManagement.DataAccessLayer/Db/ApplicationDbContext.cs
+++ eShopManagement.DataAccessLayer/Db/ApplicationDbContext.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccessLayer.EntitiesConfiguration;
+using eShopManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccessLayer.Data
+namespace eShopManagement.DataAccessLayer.Data
 {
     public class ApplicationDbContext : DbContext
     {
@@ -17,13 +14,32 @@ namespace DataAccessLayer.Data
             : base(options)
         {
         }
-
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            SeedData(modelBuilder);
             base.OnModelCreating(modelBuilder);
 
         }
-        
+        private static void SeedData(ModelBuilder modelBuilder)
+        {
+            var productsJsonPath = Path.Combine(Directory.GetCurrentDirectory(), "SeedData", "products.json");
+
+            // Seed Products
+            if (File.Exists(productsJsonPath))
+            {
+                var productsJson = File.ReadAllText(productsJsonPath);
+                var products = JsonConvert.DeserializeObject<List<Product>>(productsJson);
+                if (products != null && products.Any())
+                {
+                    modelBuilder.Entity<Product>().HasData(products);
+                }
+            }
+        }
+
     }
 }
